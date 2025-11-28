3342eddf50336badd9e9bc8ffc98b14b36e568b7
Elielz_lladosjr <elielzetasoto285@gmail.com>
2025-11-28 21:15:24 +0100

    ahora si ultima jaja

--- mi-pokedex/src/pages/Digimon.tsx
+++ mi-pokedex/src/pages/Digimon.tsx
@@ -9,17 +9,16 @@ export default function Digimon() {
       const data = await fetch("https://digimon-api.vercel.app/api/digimon")
         .then(r => r.json());
 
-      // Seleccionamos 10 aleatorios y agregamos stats simulados
       const random10 = Array.from({ length: 10 }, () => {
         const index = Math.floor(Math.random() * data.length);
         const digi = data[index];
 
         return {
           ...digi,
-          hp: Math.floor(Math.random() * 100) + 50,        // HP entre 50 y 149
-          attack: Math.floor(Math.random() * 50) + 20,     // Ataque entre 20 y 69
-          defense: Math.floor(Math.random() * 50) + 20,    // Defensa entre 20 y 69
-          xp: Math.floor(Math.random() * 1000) + 100,      // XP entre 100 y 1099
+          hp: Math.floor(Math.random() * 100) + 50,        
+          attack: Math.floor(Math.random() * 50) + 20,     
+          defense: Math.floor(Math.random() * 50) + 20,    
+          xp: Math.floor(Math.random() * 1000) + 100,      
         };
       });
 
